d558653a07b81c90cd3e092f2b3f3bf40abbd947
Florian Jourda <florian@box.com>
2014-08-16 23:59:42 -0700

    Made ParalleLinesNodeMaskSequence accept different directions

--- src/heronarts/lx/nodemask/sequence/ParalleLinesNodeMaskSequence.java
+++ src/heronarts/lx/nodemask/sequence/ParalleLinesNodeMaskSequence.java
@@ -1,5 +1,6 @@
 package heronarts.lx.nodemask.sequence;
 
+import com.sun.tools.javac.code.Attribute;
 import heronarts.lx.HeronLX;
 import heronarts.lx.nodemask.LineNodeMask;
 import heronarts.lx.nodemask.NodeMask;
@@ -16,20 +17,57 @@ import java.util.Set;
 public class ParalleLinesNodeMaskSequence extends NodeMaskSequence {
 
 	private NodeMask[] nodeMaskArray;
+	private PVector sequenceDirection;
 
-	public ParalleLinesNodeMaskSequence(HeronLX lx) {
-		this(lx, lx.width, lx.height);
+	public ParalleLinesNodeMaskSequence(HeronLX lx, PVector sequenceDirection) {
+		this(lx, sequenceDirection, lx.width, lx.height);
 	}
 
-	public ParalleLinesNodeMaskSequence(HeronLX lx, int width, int height) {
-		// @TODO(florian.jourda): give more options about direction
+	/**
+	 * @param lx
+	 * @param sequenceDirection
+	 * @param width
+	 * @param height
+	 * @throws java.lang.Exception
+	 */
+	public ParalleLinesNodeMaskSequence(HeronLX lx, PVector sequenceDirection, int width, int height) {
 		super();
-		this.nodeMaskArray = new NodeMask[width];
-		for (int x = 0; x < width; x++) {
-			PVector top = new PVector(x, 0);
-			PVector bottom = new PVector(x, height - 1);
-			LineNodeMask nodeMask = new LineNodeMask(lx, top, bottom);
-			this.nodeMaskArray[x] = nodeMask;
+		this.sequenceDirection = sequenceDirection;
+		// @TODO(florian.jourda@gmail.com): make the code DRY once the API is stabilized
+		if (sequenceDirection == new PVector(1, 0)) {
+			this.nodeMaskArray = new NodeMask[width];
+			for (int x = 0; x < width; x++) {
+				PVector top = new PVector(x, 0);
+				PVector bottom = new PVector(x, height - 1);
+				LineNodeMask nodeMask = new LineNodeMask(lx, top, bottom);
+				this.nodeMaskArray[x] = nodeMask;
+			}
+		} else if (sequenceDirection == new PVector(-1, 0)) {
+			this.nodeMaskArray = new NodeMask[width];
+			for (int x = 0; x < width; x++){
+				PVector top = new PVector(x, 0);
+				PVector bottom = new PVector(x, height - 1);
+				LineNodeMask nodeMask = new LineNodeMask(lx, top, bottom);
+				this.nodeMaskArray[width - 1 - x] = nodeMask;
+			}
+		} else if (sequenceDirection == new PVector(0, 1)) {
+			this.nodeMaskArray = new NodeMask[height];
+			for (int y = 0; y < height; y++) {
+				PVector left = new PVector(0, y);
+				PVector right = new PVector(width - 1, y);
+				LineNodeMask nodeMask = new LineNodeMask(lx, left, right);
+				this.nodeMaskArray[y] = nodeMask;
+			}
+		} else if (sequenceDirection == new PVector(1, 0)) {
+			this.nodeMaskArray = new NodeMask[height];
+			for (int y = 0; y < height; y++) {
+				PVector left = new PVector(0, y);
+				PVector right = new PVector(width - 1, y);
+				LineNodeMask nodeMask = new LineNodeMask(lx, left, right);
+				this.nodeMaskArray[height - 1 - y] = nodeMask;
+			}
+		} else {
+			//throw new Exception("sequenceDirection should be {1 ,0}, {-1, 0}, {0, 1} or {0, -1}");
 		}
 		this.length = this.nodeMaskArray.length;
 	}
